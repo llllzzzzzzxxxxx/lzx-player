d5b3bdb9085e57ac77bf16ea0cf338dc2d585151
llllzzzzzzxxxxx <487793904@qq.com>
2025-03-08 21:10:36 +0800

    构建项目

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  base: '/lzx-player/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
